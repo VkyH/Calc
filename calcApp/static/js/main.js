034fd20558b55170344231e9d07b6bba51298871
VkyH <hvicky2003@gmail.com>
2022-09-15 00:50:24 +0530

    javascript file not found fixed

--- calcApp/static/js/main.js
+++ calcApp/static/js/main.js
@@ -1,24 +1,23 @@
-const socket=new WebSocket("ws://localhost:8000/ws/calc/")
-        socket.onmessage=(e)=>{
-            result=JSON.parse(e.data).result;
-            document.getElementById("result").value+=result+"\n";
-        }
-        socket.onclose=(e)=>{
-            console.log("socket closed");
-        }
-        
-        document.getElementById("expr").focus();
-        document.getElementById("expr").onkeyup=function(e){
-            if(e.keyCode===13){
-                document.getElementById('submit').click();
-                document.getElementById("expr").value="";
-            }
-        };
-        document.getElementById("submit").onclick=function(e){
-            expr=document.getElementById("expr").value
-            socket.send(JSON.stringify({
-                    expression:expr
-                }))
-            document.getElementById("result").value=expr+"= "
-        }
-        
+const socket=new WebSocket("ws://localhost:8000/ws/calc/");
+socket.onmessage=(e)=>{
+    result=JSON.parse(e.data).result;
+    document.getElementById("result").value+=result+"\n";
+}
+socket.onclose=(e)=>{
+    console.log("socket closed");
+}
+
+document.getElementById("expr").focus();
+document.getElementById("expr").onkeyup=function(e){
+    if(e.key==="Enter"){
+        document.getElementById('submit').click();
+        document.getElementById("expr").value="";
+    }
+};
+document.getElementById("submit").onclick=function(e){
+    expr=document.getElementById("expr").value
+    socket.send(JSON.stringify({
+            expression:expr
+        }))
+    document.getElementById("result").value=expr+"= "
+}
--- calcProject/settings.py
+++ calcProject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 SATICFILES_DIRS=[
-    BASE_DIR/'static'
+    'calcApp/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
